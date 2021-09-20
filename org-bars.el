83731bf04d55c2da0a10b0bc0407165c089f0d42
tony <tony.aldon.adm@gmail.com>
2021-09-20 07:14:25 +0200

    rename: org-indent-bars.el => org-bars.el
    Rename file: org-indent-bars.el => org-bars.el
    
    Update the prefix (namespace) of variables and functions accordingly:
    org-indent-bars => org-bars
    
    * `org-indent-bars-xpm-image`                  => `org-bars-xpm-image`
    * `org-indent-bars-pixel-line`                 => `org-bars-pixel-line`
    * `org-indent-bars-pixel-bar`                  => `org-bars-pixel-bar`
    * `org-indent-bars-color-level`                => `org-bars-color-level`
    * `org-indent-bars-xpm-color-spec`             => `org-bars-xpm-color-spec`
    
    * `org-indent-bars-pixel-line-test`            => `org-bars-pixel-line-test`
    * `org-indent-bars-pixel-bar-test`             => `org-bars-pixel-bar-test`
    * `org-indent-bars-color-level-test`           => `org-bars-color-level-test`
    * `org-indent-bars-xpm-color-spec-test`        => `org-bars-xpm-color-spec-test`
    
    * `org-indent-bars-star-empty`                 => `org-bars-star-empty`
    * `org-indent-bars-star-invisible`             => `org-bars-star-invisible`
    * `org-indent-bars-star-visible`               => `org-bars-star-visible`
    * `org-indent-bars-stars`                      => `org-bars-stars`
    * `org-indent-bars-subtree-is-empty-p`         => `org-bars-subtree-is-empty-p`
    * `org-indent-bars-subtree-is-invisible-p`     => `org-bars-subtree-is-invisible-p`
    * `org-indent-bars-star`                       => `org-bars-star`
    * `org-indent-bars-get-level-face`             => `org-bars-get-level-face`
    * `org-indent-bars-refresh-stars`              => `org-bars-refresh-stars`
    * `org-indent-bars-remove-replacement-stars`   => `org-bars-remove-replacement-stars`
    
    * `org-indent-bars-fix-headings-before-change` => `org-bars-fix-headings-before-change`
    * `org-indent-bars-compute-prefixes`           => `org-bars-compute-prefixes`
    * `org-indent-bars-mode`                       => `org-bars-mode`

--- org-bars.el
+++ org-bars.el
@@ -1,11 +1,11 @@
-;;; org-indent-bars-xpm-image
+;;; org-bars-xpm-image
 
-(defun org-indent-bars-xpm-image (level)
+(defun org-bars-xpm-image (level) ; maybe add arguments: width, heigth, vpadding
   "Return an image descriptor for level LEVEL in the org tree.
 
-When `org-indent-bars-mode' is on, the `line-prefix' property
+When `org-bars-mode' is on, the `line-prefix' property
 of each line at the level LEVEL (LEVEL > 0) in the org tree, is set to
-the XPM image produced by `org-indent-bars-xpm-image'.
+the XPM image produced by `org-bars-xpm-image'.
 
 LEVEL must be strickly superior to 0."
   (let* ((identifier "/* XPM */\nstatic char *rule[] = {")
@@ -18,8 +18,8 @@ LEVEL must be strickly superior to 0."
          (colors "9") ; 8 org face levels + None color
          (dimensions
           (concat "\"" width-str " " heigth-str " " colors " " character-per-pixel "\","))
-         (color-spec (org-indent-bars-xpm-color-spec 30 15))
-         (pixel-line (org-indent-bars-pixel-line level width indentation))
+         (color-spec (org-bars-xpm-color-spec 30 15))
+         (pixel-line (org-bars-pixel-line level width indentation))
          (raster (-reduce #'concat (-repeat height pixel-line)))
          (end "};")
          (data (concat identifier dimensions color-spec raster end)))
@@ -28,26 +28,28 @@ LEVEL must be strickly superior to 0."
             :mask heuristic
             :ascent center)))
 
-(defun org-indent-bars-pixel-line (level width indentation)
+;; if we want to add vertical padding to heading line we need
+;; to add one extra left None pixel to every pixel-lines
+(defun org-bars-pixel-line (level width indentation) ; maybe add argument: vpadding
   "Return the pixels line for level LEVEL with WIDTH being the character's width.
 
  (* WIDTH (- INDENTATION 1)) corresponds to the number of None pixels we add
-after each level bar.  In practice, `org-indent-bars-pixel-line' is called
+after each level bar.  In practice, `org-bars-pixel-line' is called
 with INDENTATION argument value equal to `org-indent-indentation-per-level'.
 
-`org-indent-bars-pixel-line' is used to construct the XPM image
+`org-bars-pixel-line' is used to construct the XPM image
 used as `line-prefix' text property for each line for the level
 LEVEL in the org tree."
   (let ((none-pixels (s-repeat (* (1- indentation) width) "0")))
     (concat "\""
-            (s-join none-pixels (--map (org-indent-bars-pixel-bar it width)
+            (s-join none-pixels (--map (org-bars-pixel-bar it width)
                                        (number-sequence 1 level)))
             none-pixels
             "\",")))
 
-(defun org-indent-bars-pixel-bar (level width)
+(defun org-bars-pixel-bar (level width)
   "Return WIDTH pixels equal to 0 but one centered equal to LEVEL.
-For instance (org-indent-bars-pixel-bar 3 9) -> \"000030000\"."
+For instance (org-bars-pixel-bar 3 9) -> \"000030000\"."
   (cond
    ((= width 1) (number-to-string level))
    ((= width 2) (concat "0" (number-to-string level)))
@@ -62,13 +64,13 @@ For instance (org-indent-bars-pixel-bar 3 9) -> \"000030000\"."
               (number-to-string level)
               (s-repeat r-pixels "0"))))))
 
-(defun org-indent-bars-color-level (face desaturate darken)
+(defun org-bars-color-level (face desaturate darken)
   "Desaturate and darken foreground color of FACE."
   (-> (face-foreground face nil t)
       (color-desaturate-name desaturate)
       (color-darken-name darken)))
 
-(defun org-indent-bars-xpm-color-spec (desaturate darken)
+(defun org-bars-xpm-color-spec (desaturate darken)
   "Return color specification used in XPM format.
 1 to 8 corresponds to a modified version of the
 foreground colors of the 8 faces in `org-level-faces'.
@@ -77,7 +79,7 @@ foreground colors of the 8 faces in `org-level-faces'.
    (-reduce
     #'concat
     (--map-indexed (concat "\"" (number-to-string (1+ it-index)) " c "
-                           (org-indent-bars-color-level it desaturate darken)
+                           (org-bars-color-level it desaturate darken)
                            "\",")
                    org-level-faces))
    "\"0 c None\","))
@@ -86,13 +88,13 @@ foreground colors of the 8 faces in `org-level-faces'.
 
 ;;;; comment
 
-(comment ; dotimes, number-sequence, org-indent-bars-xpm-image
- (insert (propertize " " 'display (org-indent-bars-xpm-image 1)))
- (insert (propertize " " 'display (org-indent-bars-xpm-image 2)))
- (insert (propertize " " 'display (org-indent-bars-xpm-image 3)))
- (insert (propertize " " 'display (org-indent-bars-xpm-image 4)))
- (insert (propertize " " 'display (org-indent-bars-xpm-image 5)))
- (insert (propertize " " 'display (org-indent-bars-xpm-image 17)))
+(comment ; dotimes, number-sequence, org-bars-xpm-image
+ (insert (propertize " " 'display (org-bars-xpm-image 1)))
+ (insert (propertize " " 'display (org-bars-xpm-image 2)))
+ (insert (propertize " " 'display (org-bars-xpm-image 3)))
+ (insert (propertize " " 'display (org-bars-xpm-image 4)))
+ (insert (propertize " " 'display (org-bars-xpm-image 5)))
+ (insert (propertize " " 'display (org-bars-xpm-image 17)))
 
  (dotimes (l 3) (message "%s" l))
  (number-sequence 1 5); (1 2 3 4 5)
@@ -106,6 +108,7 @@ foreground colors of the 8 faces in `org-level-faces'.
 ;;;; test
 
 ;; (global-set-key (kbd "C-<f1>") (lambda () (interactive)(ert t)))
+;; (global-set-key (kbd "C-<f1>") (lambda () (interactive)(ert "org-bars-pixel-line-test")))
 
 (comment ; manual test with different org-indent-indentation-per-level
  (setq org-indent-indentation-per-level 1)
@@ -114,29 +117,29 @@ foreground colors of the 8 faces in `org-level-faces'.
  )
 
 
-(ert-deftest org-indent-bars-pixel-line-test ()
-  (should (string= (org-indent-bars-pixel-line 3 3 1) "\"010020030\","))
-  (should (string= (org-indent-bars-pixel-line 3 4 1) "\"001000200030\","))
-  (should (string= (org-indent-bars-pixel-line 2 6 1) "\"000100000200\","))
-  (should (string= (org-indent-bars-pixel-line 2 3 1) "\"010020\","))
-  (should (string= (org-indent-bars-pixel-line 2 3 2) "\"010000020000\","))
-  (should (string= (org-indent-bars-pixel-line 2 3 3) "\"010000000020000000\",")))
+(ert-deftest org-bars-pixel-line-test ()
+  (should (string= (org-bars-pixel-line 3 3 1) "\"010020030\","))
+  (should (string= (org-bars-pixel-line 3 4 1) "\"001000200030\","))
+  (should (string= (org-bars-pixel-line 2 6 1) "\"000100000200\","))
+  (should (string= (org-bars-pixel-line 2 3 1) "\"010020\","))
+  (should (string= (org-bars-pixel-line 2 3 2) "\"010000020000\","))
+  (should (string= (org-bars-pixel-line 2 3 3) "\"010000000020000000\",")))
 
-(ert-deftest org-indent-bars-pixel-bar-test ()
-  (should (string= (org-indent-bars-pixel-bar 3 1) "3"))
-  (should (string= (org-indent-bars-pixel-bar 3 2) "03"))
-  (should (string= (org-indent-bars-pixel-bar 3 3) "030"))
-  (should (string= (org-indent-bars-pixel-bar 3 5) "00300"))
-  (should (string= (org-indent-bars-pixel-bar 3 4) "0030"))
-  (should (string= (org-indent-bars-pixel-bar 1 6) "000100")))
+(ert-deftest org-bars-pixel-bar-test ()
+  (should (string= (org-bars-pixel-bar 3 1) "3"))
+  (should (string= (org-bars-pixel-bar 3 2) "03"))
+  (should (string= (org-bars-pixel-bar 3 3) "030"))
+  (should (string= (org-bars-pixel-bar 3 5) "00300"))
+  (should (string= (org-bars-pixel-bar 3 4) "0030"))
+  (should (string= (org-bars-pixel-bar 1 6) "000100")))
 
-(ert-deftest org-indent-bars-color-level-test ()
+(ert-deftest org-bars-color-level-test ()
   (defface color-level '((t :foreground "#4dafc3")) "")
   (should
-   (string= (org-indent-bars-color-level 'color-level 30 15)
+   (string= (org-bars-color-level 'color-level 30 15)
             "#4eea6ed47558")))
 
-(ert-deftest org-indent-bars-xpm-color-spec-test ()
+(ert-deftest org-bars-xpm-color-spec-test ()
   (defface color-level-1 '((t :foreground "#4dafc3")) "")
   (defface color-level-3 '((t :foreground "#d07391")) "")
   (defface color-level-2 '((t :foreground "#c97260")) "")
@@ -144,7 +147,7 @@ foreground colors of the 8 faces in `org-level-faces'.
         (desaturate 30)
         (darken 15))
     (should
-     (string= (org-indent-bars-xpm-color-spec desaturate darken)
+     (string= (org-bars-xpm-color-spec desaturate darken)
               (concat "\"1 c #4eea6ed47558\","
                       "\"2 c #840860a45952\","
                       "\"3 c #942563507310\","
@@ -153,44 +156,44 @@ foreground colors of the 8 faces in `org-level-faces'.
 
 ;;; font lock stuff for heading lines, org-get-level-face
 
-(defface org-indent-bars-star-empty nil
+(defface org-bars-star-empty nil
   "If non trivial, this is the face used for the heading star
 when corresponding subtree is empty.  If trivial, faces in
 `org-level-faces' will be used.
 
-See `face-nontrivial-p' and `org-indent-bars-subtree-is-empty-p'.")
+See `face-nontrivial-p' and `org-bars-subtree-is-empty-p'.")
 
-(defface org-indent-bars-star-invisible nil
+(defface org-bars-star-invisible nil
   "If non trivial, this is the face used for the heading star
 when corresponding subtree is not empty and invisible.  If trivial,
 faces in `org-level-faces' will be used.
 
-See `face-nontrivial-p', `org-indent-bars-subtree-is-empty-p' and
-`org-indent-bars-subtree-is-invisible-p'.")
+See `face-nontrivial-p', `org-bars-subtree-is-empty-p' and
+`org-bars-subtree-is-invisible-p'.")
 
-(defface org-indent-bars-star-visible nil
+(defface org-bars-star-visible nil
   "If non trivial, this is the face used for the heading star
 when corresponding subtree is not visible.  If trivial, faces
 in `org-level-faces' will be used.
 
-See `face-nontrivial-p', `org-indent-bars-subtree-is-empty-p' and
-`org-indent-bars-subtree-is-invisible-p'.")
+See `face-nontrivial-p', `org-bars-subtree-is-empty-p' and
+`org-bars-subtree-is-invisible-p'.")
 
-(defvar org-indent-bars-stars
+(defvar org-bars-stars
   '(:empty "*"
     :invisible "+"
     :visible "-")
   "Plist of the strings used in place of the star \"* \" in heading lines.
 The replacement star is choosen accordingly to the state of the subtree:
 :empty
-    if the subtree is empty (see `org-indent-bars-subtree-is-empty-p'),
+    if the subtree is empty (see `org-bars-subtree-is-empty-p'),
 :invisible
     if the subtree is not empty and invisible
-    (see `org-indent-bars-subtree-is-invisible-p'),
+    (see `org-bars-subtree-is-invisible-p'),
 :visible
     if the subtree is not empty and visible.")
 
-(defun org-indent-bars-subtree-is-empty-p ()
+(defun org-bars-subtree-is-empty-p ()
   "Return t if subtree at point is empty."
   (save-match-data
     (save-excursion
@@ -201,31 +204,31 @@ The replacement star is choosen accordingly to the state of the subtree:
                                  (point))))
         (= subtree-end heading-end)))))
 
-(defun org-indent-bars-subtree-is-invisible-p ()
+(defun org-bars-subtree-is-invisible-p ()
   "Return t if subtree at point is invisible."
   (save-match-data (org-invisible-p (point-at-eol))))
 
-(defun org-indent-bars-star ()
+(defun org-bars-star ()
   "Return plist of the string and face of the star to use on heading at point.
 The plist has the keywords: :star and :face.
 
 The string and face to use depend on the state of the subtree: empty,
 invisible or visible.
 
-You can customize the stars with the variable `org-indent-bars-stars'."
+You can customize the stars with the variable `org-bars-stars'."
   (let (star face)
-    (cond ((org-indent-bars-subtree-is-empty-p)
-           (setq star (plist-get org-indent-bars-stars :empty))
-           (setq face 'org-indent-bars-star-empty))
-          ((org-indent-bars-subtree-is-invisible-p)
-           (setq star (plist-get org-indent-bars-stars :invisible))
-           (setq face 'org-indent-bars-star-invisible))
+    (cond ((org-bars-subtree-is-empty-p)
+           (setq star (plist-get org-bars-stars :empty))
+           (setq face 'org-bars-star-empty))
+          ((org-bars-subtree-is-invisible-p)
+           (setq star (plist-get org-bars-stars :invisible))
+           (setq face 'org-bars-star-invisible))
           (t
-           (setq star (plist-get org-indent-bars-stars :visible))
-           (setq face 'org-indent-bars-star-visible)))
+           (setq star (plist-get org-bars-stars :visible))
+           (setq face 'org-bars-star-visible)))
     `(:star ,star :face ,face)))
 
-(defun org-indent-bars-get-level-face (n)
+(defun org-bars-get-level-face (n)
   "Get the right face for match N in font-lock matching of headlines.
 
 This function is meant to override `org-get-level-face' with an advice."
@@ -254,17 +257,17 @@ This function is meant to override `org-get-level-face' with an advice."
      ((eq n 1)
       (or (= org-l0 1)
           (add-text-properties
-           beg-1 (- end-2 2) '(invisible org-indent-bars-invisible)))
+           beg-1 (- end-2 2) '(invisible org-bars-invisible)))
       'default)
      ((eq n 2)
-      (let* ((star (org-indent-bars-star))
+      (let* ((star (org-bars-star))
              (star-s (plist-get star :star))
              (star-f (plist-get star :face)))
         (compose-region beg-2 (1- end-2) star-s)
         (if (face-nontrivial-p star-f) star-f org-f)))
      (t (unless org-level-color-stars-only org-f)))))
 
-(defun org-indent-bars-refresh-stars (state)
+(defun org-bars-refresh-stars (state)
   "Refontify all visible heading stars each time `org-cycle' command is used.
 
 This function is meant to be added to `org-cycle-hook'."
@@ -275,8 +278,8 @@ This function is meant to be added to `org-cycle-hook'."
         (unless (car (get-char-property-and-overlay (point) 'invisible))
           (put-text-property (point-at-bol) (point-at-eol) 'fontified nil))))))
 
-(defun org-indent-bars-remove-replacement-stars ()
-  "Remove replacement stars `org-indent-bars-stars' on every heading lines."
+(defun org-bars-remove-replacement-stars ()
+  "Remove replacement stars `org-bars-stars' on every heading lines."
   (save-excursion
     (goto-char (point-min))
     (while (re-search-forward "^\\(\\**\\)\\(\\* \\)" nil t)
@@ -286,14 +289,14 @@ This function is meant to be added to `org-cycle-hook'."
 
 (comment ; for manual testing
  (custom-set-faces
-  '(org-indent-bars-star-empty ((t (:foreground "#00ff00"))))
-  '(org-indent-bars-star-invisible ((t (:foreground "#ff0000"))))
-  '(org-indent-bars-star-visible ((t (:foreground "#0000ff")))))
+  '(org-bars-star-empty ((t (:foreground "#00ff00"))))
+  '(org-bars-star-invisible ((t (:foreground "#ff0000"))))
+  '(org-bars-star-visible ((t (:foreground "#0000ff")))))
 
  (custom-set-faces
-  '(org-indent-bars-star-empty ((t nil)))
-  '(org-indent-bars-star-invisible ((t nil)))
-  '(org-indent-bars-star-visible ((t nil))))
+  '(org-bars-star-empty ((t nil)))
+  '(org-bars-star-invisible ((t nil)))
+  '(org-bars-star-visible ((t nil))))
 
  (setq org-level-color-stars-only t)
  (setq org-level-color-stars-only nil)
@@ -308,7 +311,7 @@ This function is meant to be added to `org-cycle-hook'."
 ;; remove it with `backward-delete-char-untabify', the display
 ;; with bitmap of the heading is messed up.
 
-(defun org-indent-bars-fix-headings-before-change (start end)
+(defun org-bars-fix-headings-before-change (start end)
   "Fix headings display.
 It is meant to be used in `before-change-functions'."
   (when (org-at-heading-p)
@@ -316,7 +319,7 @@ It is meant to be used in `before-change-functions'."
 
 ;;; compute prefixes
 
-(defun org-indent-bars-compute-prefixes ()
+(defun org-bars-compute-prefixes ()
   "Compute prefix strings for regular text and headlines.
 
 This function is meant to override `org-indent--compute-prefixes'
@@ -341,7 +344,7 @@ with an advice."
               n
               (if (<= n 1)
                   ""
-                (propertize " " 'display (org-indent-bars-xpm-image (1- n)))))
+                (propertize " " 'display (org-bars-xpm-image (1- n)))))
 
         ;; -------
         ;; BEG: part unchanged from `org-indent--compute-prefixes'
@@ -362,34 +365,34 @@ with an advice."
             n
             (if (= n 0)
                 ""
-              (propertize " " 'display (org-indent-bars-xpm-image n)))))))
+              (propertize " " 'display (org-bars-xpm-image n)))))))
 
-;;; org-indent-bars-mode
+;;; org-bars-mode
 
-(define-minor-mode org-indent-bars-mode
-  "Toggle `org-indent-bars-mode' mode on or off."
+(define-minor-mode org-bars-mode
+  "Toggle `org-bars-mode' mode on or off."
   :global nil
   (cond
-   (org-indent-bars-mode
-    (push 'org-indent-bars-fix-headings-before-change before-change-functions)
+   (org-bars-mode
+    (push 'org-bars-fix-headings-before-change before-change-functions)
     (advice-add 'org-indent--compute-prefixes :override
-                'org-indent-bars-compute-prefixes)
+                'org-bars-compute-prefixes)
     (advice-add 'org-get-level-face :override
-                'org-indent-bars-get-level-face)
-    (add-hook 'org-cycle-hook 'org-indent-bars-refresh-stars nil t)
-    (add-to-invisibility-spec '(org-indent-bars-invisible))
+                'org-bars-get-level-face)
+    (add-hook 'org-cycle-hook 'org-bars-refresh-stars nil t)
+    (add-to-invisibility-spec '(org-bars-invisible))
     (org-indent-mode -1)
     (org-indent-mode 1))
    (t
     (setq before-change-functions
-          (delq 'org-indent-bars-fix-headings-before-change before-change-functions))
+          (delq 'org-bars-fix-headings-before-change before-change-functions))
     (advice-remove 'org-indent--compute-prefixes
-                   'org-indent-bars-compute-prefixes)
+                   'org-bars-compute-prefixes)
     (advice-remove 'org-get-level-face
-                   'org-indent-bars-get-level-face)
-    (remove-hook 'org-cycle-hook 'org-indent-bars-refresh-stars t)
-    (org-indent-bars-remove-replacement-stars)
-    (remove-from-invisibility-spec '(org-indent-bars-invisible))
+                   'org-bars-get-level-face)
+    (remove-hook 'org-cycle-hook 'org-bars-refresh-stars t)
+    (org-bars-remove-replacement-stars)
+    (remove-from-invisibility-spec '(org-bars-invisible))
     (org-indent-mode -1))))
 
-(global-set-key (kbd "C-<f2>") 'org-indent-bars-mode)
+(global-set-key (kbd "C-<f2>") 'org-bars-mode)
