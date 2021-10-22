8b45e6b2889eeb8de47deabb2a97fe555a539704
tony <tony.aldon.adm@gmail.com>
2021-10-22 17:59:56 +0200

    add: dependencies and licence

--- org-bars.el
+++ org-bars.el
@@ -1,3 +1,39 @@
+;;; org-bars.el --- Add bars to virtual indentation -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 Tony Aldon
+
+;; Author: Tony Aldon <tony.aldon.adm@gmail.com>
+;; Package-Requires: ((emacs "27.1") (dash "2.17.0") (s "1.12.0"))
+;; Keywords: outlines
+;; Homepage: https://github.com/tonyaldon/org-bars
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO:
+
+;;; Code:
+
+(require 'color)
+(require 'face-remap)
+(require 's)
+(require 'dash)
+(require 'org-indent)
+
 ;;; compute XPM image data (virtual bars)
 
 (defvar org-bars-color-options '(:only-one-color nil)
@@ -557,3 +593,6 @@ This is meant to be used in `post-command-hook'."
     (remove-from-invisibility-spec '(org-bars))
     (org-indent-mode -1)
     (if org-bars-org-indent-mode (org-indent-mode 1)))))
+
+(provide 'org-bars)
+;;; org-bars.el ends here
